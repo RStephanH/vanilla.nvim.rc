be56beb37d38a5b451b52b373e3d7345d865b1b8
rsh <randrianirinahernandezstephan@gmail.com>
2025-11-02 22:21:16 +0300

    feat: ajust panel for undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,12 @@
 return {
 	'mbbill/undotree',
 	config = function()
-		vim.keymap.set('n', '<leader>U', vim.cmd.UndotreeToggle)
+		vim.g.undotree_WindowLayout = 4
+	vim.g.undotree_Diffpanel = 1
+	vim.g.undotree_DiffpanelHeight = 15
+	vim.g.undotree_ShortIndicators = 1
+	vim.g.undotree_SplitWidth = 40
+	vim.keymap.set('n', '<leader>U', vim.cmd.UndotreeToggle)
+
 	end,
 }
